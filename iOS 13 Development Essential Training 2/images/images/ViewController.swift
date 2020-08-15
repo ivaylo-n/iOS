e87bbddd55f8136f5728c2209670f5f73abf5855
ivaylo-n <5083752+ivaylo-n@users.noreply.github.com>
2020-08-15 16:23:32 +0200

    Add Images Proj

---
+++ iOS 13 Development Essential Training 2/images/images/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  images
+//
+//  Created by Ivaylo on 8/15/20.
+//  Copyright © 2020 Ivaylo Nedelchev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
