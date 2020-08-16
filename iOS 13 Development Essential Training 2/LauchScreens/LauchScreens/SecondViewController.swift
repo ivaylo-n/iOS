7fcd7346f836b75d0a415f0b0c052e9bd8728360
ivaylo-n <5083752+ivaylo-n@users.noreply.github.com>
2020-08-16 10:14:09 -0700

    Add LauchScreen Project

---
+++ iOS 13 Development Essential Training 2/LauchScreens/LauchScreens/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LauchScreens
+//
+//  Created by Ivaylo on 8/16/20.
+//  Copyright © 2020 Ivaylo Nedelchev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
