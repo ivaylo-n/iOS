be3e7bb09e044d9acd83d6daa1712681465dee16
ivaylo-n <5083752+ivaylo-n@users.noreply.github.com>
2020-08-16 05:21:58 -0700

    Add Tapped Project (MultipleViewers)

---
+++ iOS 13 Development Essential Training 2/MultipleViews/MultipleViews/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MultipleViews
+//
+//  Created by Ivaylo on 8/16/20.
+//  Copyright © 2020 Ivaylo Nedelchev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
