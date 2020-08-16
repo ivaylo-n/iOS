993c26b3c8e7d7d99b519a44716483c937021c86
ivaylo-n <5083752+ivaylo-n@users.noreply.github.com>
2020-08-16 10:47:26 -0700

    Add Publishing Project

---
+++ iOS 13 Development Essential Training 2/Publishing/Publishing/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Publishing
+//
+//  Created by Ivaylo Nedelchev on 8/16/20.
+
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS 13 Development Essential Training 2/Publishing/Publishing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Publishing
+//
+//  Created by Ivaylo Nedelchev on 8/16/20.
+
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
