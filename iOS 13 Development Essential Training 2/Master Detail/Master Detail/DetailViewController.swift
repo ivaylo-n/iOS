c99d3eae9dd921029a2ea369061534913eab559b
ivaylo-n <5083752+ivaylo-n@users.noreply.github.com>
2020-08-16 05:43:54 -0700

    Add Master Detail Project

---
+++ iOS 13 Development Essential Training 2/Master Detail/Master Detail/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Master Detail
+//
+//  Created by Ivaylo on 8/16/20.
+//  Copyright © 2020 Ivaylo Nedelchev. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
